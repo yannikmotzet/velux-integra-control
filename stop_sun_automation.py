019ac7f5185833a6c4d1363f38c503e44d7cff9a
yannikmotzet <yannik.motzet@outlook.com>
2022-05-08 21:03:16 +0200

    fix typo

--- stop_sun_automation.py
+++ stop_sun_automation.py
@@ -4,6 +4,6 @@ from crontab import CronTab
 working_dir = "/home/pi/"
 cron = CronTab(user=getpass.getuser())
 
-cron.remove_all(command = working_dir + 'velux-integra-control/sunriset_automation.py')
+cron.remove_all(command = working_dir + 'velux-integra-control/sunrise_automation.py')
 cron.remove_all(command = working_dir + 'velux-integra-control/sunset_automation.py')
 cron.write()
